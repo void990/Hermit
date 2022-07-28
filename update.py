b1bce47742e7ec5eeac130fd0f42fa086f40cbd6
void990 <110184248+void990@users.noreply.github.com>
2022-07-28 21:58:44 +0400

    Add files via upload

---
+++ update.py
@@ -0,0 +1,15 @@
+from rich.console import Console
+import rich
+import os, sys
+
+console = Console()
+
+update = console.input(
+"[bold white]update tool? (y/n) "
+)
+
+if update == "y":
+   console.print("[bold yellow][ Update in progress... ]")
+   os.system("git pull")
+   console.print("[bold green][ + ] You now have the latest version")
+
